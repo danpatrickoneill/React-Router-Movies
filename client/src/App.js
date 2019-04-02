5dfe5d53782649e7db555248246691822c08aa55
danpatrickoneill <dan47573@gmail.com>
2019-04-02 15:59:42 -0400

    Completed part 2 of README

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,8 @@ export default class App extends Component {
       <Router>
         <div>
           <SavedList list={this.state.savedList} />
-          <Route path="/" component={MovieList} />
-          <Route path="/moves/:id" component={Movie} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={Movie} />
         </div>
       </Router>
     );
